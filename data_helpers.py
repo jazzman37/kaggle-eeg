ce5a54dff55bd35d15498de32f3784298359bc8d
Marko Stamenovic <markostam@gmail.com>
2016-11-28 18:25:38 -0500

    first commit

---
+++ data_helpers.py
@@ -0,0 +1,166 @@
+import numpy as np
+import itertools
+import random
+import gzip
+import pickle
+import os
+import pdb
+
+def txt_to_cliques(shs_loc):
+    '''
+    creates a dictionary out of second hand songset 'cliques'
+    or groups of cover songs and returns the dict of cliques
+    based on their msd id
+    '''
+    shs = list(open(shs_loc, encoding = 'utf-8'))
+    shs = shs[14:]
+    cliques = {}
+    for ent in shs:
+        ent = ent.replace('\n','')
+        if ent[0] == '%':
+            tempKey = ent.lower()
+            cliques[tempKey] = []
+        else:
+            cliques[tempKey].append(ent.split("<SEP>")[0]+'.mp3')
+    return cliques
+
+def get_labels(cliques):
+    # get and flatten all combination of coversongs
+    positive_examples = (list(itertools.combinations(val,2)) for key,val in cliques.items())
+    positive_examples = [i for j in positive_examples for i in j]
+    positive_len_og = len(positive_examples)
+    positive_labels = [[0,1] for _ in positive_examples]
+    # generate negative examples of an equivalent length to the positive examples list
+    song_from_each_clique = (random.choice(val) for key,val in cliques.items())
+    negative_examples = itertools.combinations(song_from_each_clique,2)
+    negative_examples = list(itertools.islice(negative_examples, positive_len_og))
+    negative_labels = [[1,0] for _ in negative_examples]
+
+    x = positive_examples + negative_examples
+    y = positive_labels + negative_labels
+    return x,y
+
+def triplets_generator(cliques):
+    '''
+    creates triplets of (query,positive,negative) given a dictionary of 
+    {cliques : permutations of all songs in clique} for use in training
+    a three-pronged cover song detection cnn
+    '''
+    train_examples = {key: list(itertools.combinations(val,2)) for key,val in cliques.items()}
+    for key,val in train_examples.items():
+        val +=  [(i[1],i[0]) for i in val]
+        cliques_minus_key = [v for k,v in cliques.items() if k != key]
+        negative_songs = [i for j in cliques_minus_key for i in j]
+        for positive_pair in val:
+            for neg in negative_songs:
+                yield positive_pair+(neg,)
+
+def resevoir_sample(triplets, length_of_triplets=659554630, length_of_output=int(2e7)):
+    '''
+    function that takes a generator (triplets) which has a huge amoung of outputs 
+    and randomly samples from it in order to create a new (smaller) randomized dataset 
+    '''
+    rand_indices = random.sample(range(length_of_triplets), length_of_output)
+    output = [None]*length_of_output
+    for triplet_index, triplet in enumerate(triplets):
+        if triplet_index in rand_indices:
+            # pdb.set_trace()
+            output[rand_indices.index(triplet_index)] = triplet
+    return output
+
+def get_labels_for_samesong_test(cliques):
+    '''
+    test function that generates positive examples/labels for songs where positive
+    is just the same song and negative is different song
+    '''
+    # positive examples are the same songs
+    positive_examples = [(i,i) for j in cliques.values() for i in j]
+    positive_len_og = len(positive_examples)
+    positive_labels = [[0,1] for _ in positive_examples]
+    # negative examples are any other song combination
+    song_from_each_clique = (random.choice(val) for key,val in cliques.items())
+    negative_examples = itertools.combinations(song_from_each_clique,2)
+    negative_examples = list(itertools.islice(negative_examples, positive_len_og))
+    negative_labels = [[1,0] for _ in negative_examples]
+
+    x = positive_examples + negative_examples
+    y = positive_labels + negative_labels
+    return x,y
+
+def batch_iter(data, batch_size, num_epochs, shuffle=True):
+    """
+    Generates a batch iterator for a dataset.
+    """
+    data = np.array(data)
+    data_size = len(data)
+    num_batches_per_epoch = int(data_size/batch_size) + 1
+    for epoch in range(num_epochs):
+        # Shuffle the data at each epoch
+        if shuffle:
+            shuffled_data = np.random.permutation(data)
+        else:
+            shuffled_data = data
+        for batch_num in range(num_batches_per_epoch):
+            start_index = batch_num * batch_size
+            end_index = min((batch_num + 1) * batch_size, data_size)
+            yield shuffled_data[start_index:end_index]
+
+def read_from_pickles(path_to_pickles):
+    '''
+    function that loads a dictionary of filename : cqt matrix
+    from a directory of gzipped pickles containing the chunked data
+    '''
+    spect_dict = {}
+    for file in os.listdir(path_to_pickles):
+        if file.endswith('.pickle.gz'):
+            with gzip.open(os.path.join(path_to_pickles,file),'rb') as f:
+                temp_dict = pickle.load(f, encoding='latin-1')
+            spect_dict.update(temp_dict)
+    return spect_dict
+
+def prune_cliques(cliques,spect_dict):
+    '''
+    prunes the coversong cliques wrt the spectrogram dictionary
+    in case any spectrograms don't exist due to data loss e.g.
+    scraping errors, filesize=0, etc...
+    '''
+    pruned_cliques={}
+    for clique_key, clique_val in cliques.items():
+        pruned_cliques[clique_key]=[]
+        for songid in clique_val:
+            if songid in spect_dict.keys():
+                pruned_cliques[clique_key].append(songid)
+        if len(pruned_cliques[clique_key]) < 2:
+            del pruned_cliques[clique_key]
+    return pruned_cliques
+
+def cliques_to_dev_train(cliques,percent_dev):
+    '''
+    splits all cliques into dev/train sets so as to not have
+    overlapping songs in dev/train to prevent overfitting
+    '''
+    dev_len = int(len(cliques)*percent_dev)
+    cliques_list = list(cliques.items())
+    train_cliques = dict(cliques_list[:-dev_len])
+    dev_cliques = dict(cliques_list[-dev_len:])
+    return train_cliques, dev_cliques
+
+def randomly_shuffle_xy_data(x,y):
+    # Randomly shuffle data
+    np.random.seed(420)
+    if len(x) != len(y):
+        raise Exception
+    shuffle_indices = np.random.permutation(np.arange(len(y)))
+    x,y = np.array(x), np.array(y)
+    x_shuffled = x[shuffle_indices]
+    y_shuffled = y[shuffle_indices]
+    return x_shuffled, y_shuffled
+
+def binarize_cliques(cliques):
+    '''
+    removes any more than two songs per clique to prevent overfitting
+    '''
+    binary_cliques={}
+    for clique_key, clique_val in cliques.items():
+        binary_cliques[clique_key]=random.sample(cliques[clique_key],2)
+    return binary_cliques
---
+++ preprocess_eeg.py
@@ -0,0 +1,155 @@
+import numpy as np
+import librosa
+import os
+import pickle
+import gzip
+import sys
+# ignore deprecation warnings caused by librosa 0.4.3 cqt with numpy
+import warnings 
+warnings.filterwarnings("ignore")
+
+'''
+functions to preprocess audio i.e. extract features for classification
+uses a log-power CQT encoded into a uint16 numpy matrix to save space
+contains both pyspark functions for running on clusters/multiple threads
+and vanilla python functions
+'''
+
+def feature_extract(songfile_name):
+	'''
+	takes: filename
+	outputs: audio feature representation from that file (currently cqt)
+	**assumes working directory contains raw song files**
+	returns a tuple containing songfile name and numpy array of song features
+	'''
+	song_loc = os.path.abspath(songfile_name)
+	y, sr = librosa.load(song_loc)
+	desire_spect_len = 2580
+	C = librosa.cqt(y=y, sr=sr, hop_length=512, fmin=None,
+					n_bins=84, bins_per_octave=12, tuning=None,
+					filter_scale=1, norm=1, sparsity=0.01, real=False)
+	# get log-power spectrogram with noise floor of -80dB
+	C = librosa.logamplitude(C**2, ref_power=np.max)
+	# scale log-power spectrogram to positive integer value for smaller footpint
+	noise_floor_db = 80
+	scaling_factor = (2**16 - 1)/noise_floor_db
+	C += noise_floor_db
+	C *= scaling_factor
+	C = C.astype('uint16')
+	# if spectral respresentation too long, crop it, otherwise, zero-pad
+	if C.shape[1] >= desire_spect_len:
+	    C = C[:,0:desire_spect_len]
+	else:
+		C = np.pad(C,((0,0),(0,desire_spect_len-C.shape[1])), 'constant')
+	return songfile_name, C
+
+'''
+vanilla python functions
+'''
+
+def create_feature_matrix(song_folder):
+	'''
+	takes: song folder filled with mp3 files as input
+	outputs: key-value pairs of filenames : feature representations (spectrograms)
+    list of filenames that caused exceptions
+	'''
+	feature_matrix = {}
+	exceptions = []
+	for filename in os.listdir(song_folder):
+		if filename.endswith(".mp3"):
+			try:
+				print("Processing: ", filename, end="\r")
+				name, features = feature_extract(os.path.join(song_folder,filename))
+				feature_matrix[name] = features
+			except:
+				print("Exception on: ", filename)
+				exceptions.append(filename)
+	return feature_matrix, exceptions
+
+def save_feature_matrix(song_folder,save_path):
+	fm,excepts = create_feature_matrix(song_folder)
+	fileHandle = gzip.open(save_path, "wb")
+	pickle.dump(fm, fileHandle)
+	fileHandle.close()
+
+'''
+pyspark functions 
+'''
+
+def create_feature_matrix_spark(song_files):
+	# cqt wrapper
+	def log_cqt(y,sr):
+		C =  librosa.cqt(y=y, sr=sr, hop_length=512, fmin=None, 
+		n_bins=84, bins_per_octave=12, tuning=None,
+		filter_scale=1, norm=1, sparsity=0.01, real=True)
+		# get log-power spectrogram with noise floor of -80dB
+		C = librosa.logamplitude(C**2, ref_power=np.max)
+		# scale log-power spectrogram to positive integer value for smaller footpint
+		noise_floor_db = 80
+		scaling_factor = (2**16 - 1)/noise_floor_db
+		C += noise_floor_db
+		C *= scaling_factor
+		C = C.astype('uint16')
+		return C
+	# padding wrapper
+	def padding(C,desired_spect_len):
+		if C.shape[1] >= desired_spect_len:
+			C = C[:,0:desired_spect_len]
+		else:
+			C = np.pad(C,((0,0),(0,desired_spect_len-C.shape[1])), 'constant')
+		return C
+	# load try-catch wrapper
+	def try_load(filename):
+		try:
+			sys.stdout.write('Processing: %s \r' % os.path.basename(filename))
+			sys.stdout.flush()
+			return librosa.load(filename)
+		except:
+			pass
+	# transormations
+	filesRDD = sc.parallelize(song_files)
+	rawAudioRDD = filesRDD.map(lambda x: (os.path.basename(x),try_load(x))).filter(lambda x: x[1] != None)
+	rawCQT = rawAudioRDD.map(lambda x: (x[int(0)], log_cqt(x[int(1)][int(0)],x[int(1)][int(1)])))
+	paddedCQT = rawCQT.map(lambda x: (x[0],padding(x[1],2580)))
+	return paddedCQT.collect()
+
+def save_feature_matrix_spark(song_files,save_path,save_name):
+	fm = create_feature_matrix_spark(song_files)
+	fm = {i[0]:i[1] for i in fm}
+	with gzip.open(os.path.join(save_path,save_name), "wb") as fileHandle:
+		pickle.dump(fm, fileHandle)
+
+def process_chunks(song_folder, save_path, overwrite = False, num_chunks=20, train_or_test="train"):
+    '''
+    chunk feature matrix creation to deal with memory constraints
+    will split the output files in to num_chunks chunks
+    '''
+    files = [os.path.join(song_folder,filename) for filename in os.listdir(song_folder) if filename.endswith(".mp3")]
+    chunk_size = int(len(files)/num_chunks)
+    j=0
+    for i in range(0, len(files), chunk_size):
+        j+=1
+        save_name = '{}_set_cqt{}.pickle.gz'.format(train_or_test,j)
+        if not os.path.isfile(os.path.join(save_path,save_name)) or overwrite:
+        	save_feature_matrix_spark(files[i:i + chunk_size],save_path,save_name)
+        else:
+        	print('{} already exists. Skipping file.'.format(save_name))
+
+# cluster
+# song_folder = '/scratch/mss460/shs/shs_train'
+# save_path = '/home/mss460/training_set_cqt.pickle'
+# hadoop song folder
+hadoop_song_folder = '/user/mss460/shs/shs_train'
+
+# local
+song_folder = '/Users/markostamenovic/Desktop/shs_train'
+save_path = '/Users/markostamenovic/Desktop/shs_train_pickles'
+
+# run with command:
+# spark-submit --driver-memory 8g --conf spark.driver.maxResultSize=2g preprocess_audio.py
+
+if __name__ == "__main__":
+	from pyspark import SparkContext
+	sc = SparkContext()
+	print("Executing as main program")
+	process_chunks(song_folder, save_path, overwrite = False, num_chunks=20, train_or_test="train")
