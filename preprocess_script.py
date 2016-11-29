820cdfa3f6bafd3bb766e6aa6ab90061adedf469
Marko Stamenovic <markostam@gmail.com>
2016-11-29 17:42:13 -0500

    updated preprocess scripts

---
+++ preprocess_script.py
@@ -0,0 +1,6 @@
+import preprocess_eeg
+
+preprocess_eeg.process_chunks("../raw_files/train_1", "../processed_files/train_1")
+preprocess_eeg.process_chunks("../raw_files/train_2", "../processed_files/train_2")
+preprocess_eeg.process_chunks("../raw_files/train_3", "../processed_files/train_3")
+preprocess_eeg.process_chunks("../raw_files/train_4", "../processed_files/pickled_files_4")
